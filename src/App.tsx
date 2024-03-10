0ee334e3c8ac773510c4bb6a1b3de1a62f3240f7
SergeyIankowski <garret933@gmail.com>
2024-03-10 10:05:05 +0100

    feat: create root store and add it through context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
+import { RootStoreContext } from "./store/hooks/root-store-context";
+import { rootStore } from "./store/root-store";
 
 function App() {
-  return <>Hello World!</>;
+  return <RootStoreContext.Provider value={rootStore}>Hello World!</RootStoreContext.Provider>;
 }
 
 export default App;
