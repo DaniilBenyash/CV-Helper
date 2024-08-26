a1d06d9cbf238a2e79c7357b7d5a753cd324dcfc
SergeiYankowsky <sergei.yankovsky@innowise.com>
2024-08-26 13:51:31 +0200

    refactor: replace DocumentInput to modules folder

--- src/components/DocumentInput/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export { findDatesAndTechnologies } from "./findDatesAndTechnologies";
-export { findProjectsData } from "./findProjectsData";
---
+++ src/modules/components/DocumentInput/utils/index.ts
@@ -0,0 +1,1 @@
+export { findProjectsData } from "./findProjectsData";
