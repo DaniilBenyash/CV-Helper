391b67cdba1adf6b792c425dab98f73da575ac29
SergeiYankowsky <sergei.yankovsky@innowise.com>
2024-09-10 10:23:20 +0200

    fix: fix name of directory

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -10,7 +10,7 @@ import { Spinner } from "@/ui-kit/Spinner";
 import { TableLink } from "@/components/TableLink";
 import { RefetchDataButton } from "@/components/RefetchDataButton";
 import { DocumentInput } from "@/modules/components/DocumentInput";
-import { GenerateBrightboxFormatDocumentButton } from "@/components/GenerateBrightBoxDocumentButton";
+import { GenerateBrightboxFormatDocumentButton } from "@/components/GenerateBrightBoxFormatDocumentButton";
 
 const isEmpty = <T extends object>(obj: T) => Object.keys(obj).length === 0;
 
